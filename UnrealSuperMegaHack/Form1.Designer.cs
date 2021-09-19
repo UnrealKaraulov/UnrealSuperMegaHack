39931ce4c41e5b7917abc00c41de0b03a4e091ab
Unreal Karaulov <88730285+UnrealKaraulov@users.noreply.github.com>
2021-09-19 14:35:48 +0300

    Initial commit

---
+++ UnrealSuperMegaHack/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace UnrealSuperMegaHack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose ( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose( );
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent ( )
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(216, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "d3scene.ru";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(23, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(220, 30);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Сделать туман более туманным\r\nИ показать то что нельзя показывать";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(23, 61);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(197, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Убрать туман из игры полностью";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(25, 96);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(144, 17);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Text = "Сдвинуть текст на 1мм";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(25, 128);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(142, 17);
+            this.checkBox4.TabIndex = 1;
+            this.checkBox4.Text = "Сделать всех черными";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 165);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ETO SUPER HACK OT ABSOL\'A!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ UnrealSuperMegaHack/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+namespace UnrealSuperMegaHack
+{
+    public partial class Form1 : Form
+    {
+        public Form1 ( )
+        {
+            InitializeComponent( );
+        }
+
+        private void button1_Click ( object sender , EventArgs e )
+        {
+            MessageBox.Show( "Что бы попасть на сайт d3scene.ru\n войдите в браузер и введите адрес сайта!" );
+        }
+        ProcessMemory war3processmemory = null;
+
+
+        private void checkBox1_CheckedChanged ( object sender , EventArgs e )
+        {
+            try
+            {
+                if ( war3processmemory == null || !war3processmemory.CheckProcess( ) )
+                {
+                    int war3proclen = Process.GetProcessesByName( "war3" ).Length;
+                    if ( war3proclen == 0 )
+                    {
+                        war3processmemory = null;
+                    }
+                    else
+                    {
+                        ProcessMemory war3mem =
+                            new ProcessMemory( Process.GetProcessesByName( "war3" ) [ 0 ].Id , "war3" );
+                        war3mem.StartProcess( );
+                        war3processmemory = war3mem;
+                    }
+                }
+            }
+            catch
+            {
+                war3processmemory = null;
+            }
+
+
+
+            try
+            {
+                if ( war3processmemory != null )
+                {
+
+                    if ( !war3processmemory.CheckProcess( ) )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                    int GameDll = war3processmemory.DllImageAddress( "Game.dll" );
+
+                    if ( GameDll == 0 )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+
+
+                    int ubratbtuman = checkBox1.Checked ? 1 : 0;
+
+                    war3processmemory.WriteInt( 0xADB0A0 + GameDll , ubratbtuman );
+
+                }
+            }
+            catch
+            {
+
+            }
+
+
+          /**/
+        }
+
+        private void checkBox2_CheckedChanged ( object sender , EventArgs e )
+        {
+            try
+            {
+                if ( war3processmemory == null || !war3processmemory.CheckProcess( ) )
+                {
+                    int war3proclen = Process.GetProcessesByName( "war3" ).Length;
+                    if ( war3proclen == 0 )
+                    {
+                        war3processmemory = null;
+                    }
+                    else
+                    {
+                        ProcessMemory war3mem =
+                            new ProcessMemory( Process.GetProcessesByName( "war3" ) [ 0 ].Id , "war3" );
+                        war3mem.StartProcess( );
+                        war3processmemory = war3mem;
+                    }
+                }
+            }
+            catch
+            {
+                war3processmemory = null;
+            }
+
+
+
+            try
+            {
+                if ( war3processmemory != null )
+                {
+
+                    if ( !war3processmemory.CheckProcess( ) )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                    int GameDll = war3processmemory.DllImageAddress( "Game.dll" );
+
+                    if ( GameDll == 0 )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                
+
+                    int ubratbtuman = checkBox2.Checked ? 1 : 0;
+
+                    war3processmemory.WriteInt( 0xACC708 + GameDll , ubratbtuman );
+
+                }
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void checkBox3_CheckedChanged ( object sender , EventArgs e )
+        {
+            try
+            {
+                if ( war3processmemory == null || !war3processmemory.CheckProcess( ) )
+                {
+                    int war3proclen = Process.GetProcessesByName( "war3" ).Length;
+                    if ( war3proclen == 0 )
+                    {
+                        war3processmemory = null;
+                    }
+                    else
+                    {
+                        ProcessMemory war3mem =
+                            new ProcessMemory( Process.GetProcessesByName( "war3" ) [ 0 ].Id , "war3" );
+                        war3mem.StartProcess( );
+                        war3processmemory = war3mem;
+                    }
+                }
+            }
+            catch
+            {
+                war3processmemory = null;
+            }
+
+
+
+            try
+            {
+                if ( war3processmemory != null )
+                {
+
+                    if ( !war3processmemory.CheckProcess( ) )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                    int GameDll = war3processmemory.DllImageAddress( "Game.dll" );
+
+                    if ( GameDll == 0 )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+
+
+                    int ubratbtuman = checkBox3.Checked ? 1 : 0;
+
+                    war3processmemory.WriteInt( 0xADF15C + GameDll , ubratbtuman );
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click ( object sender , EventArgs e )
+        {
+         
+        }
+
+        private void checkBox4_CheckedChanged ( object sender , EventArgs e )
+        {
+            try
+            {
+                if ( war3processmemory == null || !war3processmemory.CheckProcess( ) )
+                {
+                    int war3proclen = Process.GetProcessesByName( "war3" ).Length;
+                    if ( war3proclen == 0 )
+                    {
+                        war3processmemory = null;
+                    }
+                    else
+                    {
+                        ProcessMemory war3mem =
+                            new ProcessMemory( Process.GetProcessesByName( "war3" ) [ 0 ].Id , "war3" );
+                        war3mem.StartProcess( );
+                        war3processmemory = war3mem;
+                    }
+                }
+            }
+            catch
+            {
+                war3processmemory = null;
+            }
+
+
+
+            try
+            {
+                if ( war3processmemory != null )
+                {
+
+                    if ( !war3processmemory.CheckProcess( ) )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                    int GameDll = war3processmemory.DllImageAddress( "Game.dll" );
+
+                    if ( GameDll == 0 )
+                    {
+                        war3processmemory = null;
+
+                        return;
+                    }
+
+
+                    int ingame1 = war3processmemory.ReadInt( 0xACF678 + GameDll );
+                    int ingame2 = war3processmemory.ReadInt( 0xAB62A4 + GameDll );
+
+                    if ( ingame1 <= 0 && ingame2 <= 0 )
+                    {
+
+                        return;
+                    }
+
+
+
+                    int GlobalUnitClass = war3processmemory.ReadInt( GameDll + 0xAB4F80 );
+                    int UnitsArrayClassAddr = war3processmemory.ReadInt( GlobalUnitClass + 0x3BC );
+
+                    int UnitsCount = war3processmemory.ReadInt( UnitsArrayClassAddr + 0x604 );
+                    int UnitsArrayAddr = war3processmemory.ReadInt( UnitsArrayClassAddr + 0x608 );
+
+                    for ( int i = 0 ; i < UnitsCount ; i++ )
+                    {
+                        int CurrentUnitId = i;
+                        int CurrentUnitAdress = UnitsArrayAddr + CurrentUnitId * 4;
+                        CurrentUnitAdress = war3processmemory.ReadInt( CurrentUnitAdress );
+                        uint CurrentUnitFlag = war3processmemory.ReadUInt( CurrentUnitAdress + 724 );
+                        war3processmemory.WriteUInt( CurrentUnitAdress + 724 , checkBox4.Checked ? 0xFF000000 : 0xFFFFFFFF );
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
